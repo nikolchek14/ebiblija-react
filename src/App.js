bc1c551a8fa325993e94ed5eeb04b6ff523be81a
Nikolche Kolevski <nikolche.kolevski@outlook.com>
2021-01-03 22:24:21 +0100

    add data json and demo read

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Biblija from './Biblija.json';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          {Biblija[0].voved}
         </a>
       </header>
     </div>
