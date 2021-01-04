afbfb3d1393284a71b86098de0b9b4cfd184e16b
Nikolche Kolevski <nikolche.kolevski@outlook.com>
2021-01-04 22:30:05 +0100

    adding glava

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import logo from './logo.svg';
 import './App.css';
 import Biblija from './Biblija.json';
 import Sodrzina from './Sodrzina.js';
+import Glava from './Glava.js';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
-      <Sodrzina />
+      <Route component={Sodrzina} path="/" exact />
+      <Route component={Glava} path="/glava" />
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Glava.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Glava() {
+  return <h2>glava</h2>;
+}
