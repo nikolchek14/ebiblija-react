9d2a738b1e986236b24b02c2d767bcc34744d320
Nikolche Kolevski <nikolche.kolevski@outlook.com>
2021-01-04 22:40:28 +0100

    adding base osnova component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Biblija from './Biblija.json';
-import Sodrzina from './Sodrzina.js';
-import Glava from './Glava.js';
+import Osnova from './Osnova.js';
 
 function App() {
   return (
-  <BrowserRouter>
     <div className="App">
-      <Route component={Sodrzina} path="/" exact />
-      <Route component={Glava} path="/glava" />
+      <Osnova />
     </div>
-    </BrowserRouter>
   );
 }
 
---
+++ src/Osnova.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Osnova() {
+  return <h2>osnova</h2>;
+}
